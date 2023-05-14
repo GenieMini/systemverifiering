6da22acfde9a0e36dc8a064c83a2aa9102891a3a
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2023-05-14 15:25:23 +0200

    sorted tests w/ comments
    Sorted tests w/ comments

--- frontend/cypress/e2e/test_todo.cy.js
+++ frontend/cypress/e2e/test_todo.cy.js
@@ -12,16 +12,20 @@ function createUser() {
 }
 
 function loginUser() {
+    // Visit localhost:3000 (or corresponding url)
     cy.visit('http://localhost:3000/')
-
+    // Enter Email in email field
     cy.get('#email').type("test@test.com")
-
+    // Click “login”
     cy.get('form').submit()
 }
 
 function createTask() {
+    // Enter Title in title field
     cy.get("#title").type("test")
+    // Enter URL in url field
     cy.get("#url").type("QH2-TGUlwu4")
+    // Click “create”
     cy.get("form").submit()
 }
 
@@ -50,41 +54,37 @@ describe('Users task testing', () => {
         deleteUser()
     })
 
-    // R8UC1-1
+    // R8UC1
     it('Test user inputs description', () => {
-        // First type the task name.
+        // Enter Label in ‘New todo’ field
         cy.get('.inline-form').find('input[type=text]').type('New todo')
-        // Submit task
+        // Click “create”
         cy.get('.inline-form').find('input[type=submit]').click()
-        // Check if task is there
+        // Check that there are 2 ‘todo’-items
         cy.get('.todo-item').should(list => expect(list).to.have.length(2))
+        // Check that the “create” button is disabled
+        cy.get('.inline-form').find('input[type=submit]').should('be.disabled')
     });
 
     // R8UC3
     it('Test remove todo', () => {
+        // Check that there are 2 ‘todo’-items
+        cy.get('.todo-item').should(list => expect(list).to.have.length(2))
+        // Press remove on first ‘todo’
         cy.get('.remover').first().click().click()
+        // Check that there is 1 ‘todo’-item
         cy.get('.todo-item').should(list => expect(list).to.have.length(1))
     });
 
-    // R8UC1-2
-    it('Test user inputs "add" button disabled', () => {
-        // check if diabled
-        cy.get('.inline-form').find('input[type=submit]').should('be.disabled')
-    });
-
-    // R8UC2-1
+    // R8UC2
     it('Test checker unchecked', () => {
+        // Check that the first checkbox is unchecked
         cy.get('.checker').first().should('have.class', 'unchecked')
-    })
-
-    // R8UC2-1.5
-    it('Test checker checked', () => {
+        // Click the first checkbox
         cy.get('.checker').first().click().click()
+        // Check that the first checkbox is checked
         cy.get('.checker').first().should('have.class', 'checked')
-    })
-
-    // R8UC2-2
-    it('Test editable for line through', () => {
+        // Check that the text of the first ‘todo’ has stricken through text-decoration
         cy.get('.todo-list .editable').first().should('have.css', 'text-decoration-line').and('include', 'line-through')
     })
 
