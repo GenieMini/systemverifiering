f312c89132f34b3ad9f68cb15b5788fb66e6d8c9
GenieMini <zakaharif@hotmail.se>
2023-05-16 21:04:10 +0200

    update unit test

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,5 +1,6 @@
 import pytest
 import unittest.mock as mock
+from unittest.mock import patch
 from src.controllers.usercontroller import UserController
 
 TEST_DAO_USER = {
@@ -12,6 +13,9 @@ TEST_DAO_USER = {
 }
 
 def test_raise_invalid_email():
+    """
+        Checks for a raise ValueError when an invalid email string
+    """
     mocked_object = mock.MagicMock()
     mocked_object.find.return_value = [TEST_DAO_USER]
     controller = UserController(dao=mocked_object)
@@ -20,16 +24,31 @@ def test_raise_invalid_email():
     with pytest.raises(ValueError):
         controller.get_user_by_email(email=invalid_email)
 
-def test_dont_find_user():
+def test_empty_string():
+    """
+        Raise error on empty string
+    """
     mocked_object = mock.MagicMock()
-    mocked_object.find.return_value = [None]
-    controller = UserController(dao=mocked_object)
+    sut = UserController(dao=mocked_object)
+    email = ''
 
-    dont_find_email = "dont_find@gmail.com"
-    result = controller.get_user_by_email(email=dont_find_email)
-    assert result == None
+    with pytest.raises(Exception):
+        sut.get_user_by_email(email)
+
+def test_no_input():
+    """
+        Raise error on no input
+    """
+    mocked_object = mock.MagicMock()
+    sut = UserController(dao=mocked_object)
+
+    with pytest.raises(Exception):
+        sut.get_user_by_email()
 
 def test_find_user():
+    """
+        Return correct  on correct input
+    """
     mocked_object = mock.MagicMock()
     mocked_object.find.return_value = [TEST_DAO_USER]
     uc = UserController(dao=mocked_object)
@@ -37,3 +56,54 @@ def test_find_user():
     valid_email = "test@gmail.se"
     result = uc.get_user_by_email(email=valid_email)
     assert result == TEST_DAO_USER
+
+def test_dont_find_user():
+    """
+        Raise error when email does not exist in dao
+    """
+    mocked_object = mock.MagicMock()
+
+    dont_find_email = "dontfind@gmail.se"
+    valid_email = "valid@gmail.se"
+    mocked_object.find.return_value = [{'email': valid_email}]
+    uc = UserController(dao=mocked_object)
+
+    result = uc.get_user_by_email(email=dont_find_email)
+    assert result == None
+
+def test_nothing_in_dao():
+    """
+        Raise error in case DAO has nothing
+    """
+    mocked_object = mock.MagicMock()
+    mocked_object.find.return_value = []
+    uc = UserController(dao=mocked_object)
+
+    valid_email = "test@gmail.se"
+    result = uc.get_user_by_email(email=valid_email)
+    assert result == None
+
+@patch('builtins.print')
+def test_duplicate_emails(mock_print):
+    """
+        Raise an exception incase there are duplicate emails
+    """
+    mocked_object = mock.MagicMock()
+    email = TEST_DAO_USER["email"]
+    mocked_object.find.return_value = [TEST_DAO_USER, TEST_DAO_USER]
+    sut = UserController(dao=mocked_object)
+    sut.get_user_by_email(email)
+
+    mock_print.assert_called_with(f'Error: more than one user found with mail {email}')
+
+def test_database_operation_fails():
+    """
+        Check if get_user_by_email raises an exception when database connection/operation fails.
+    """
+    mocked_object = mock.MagicMock()
+    email = TEST_DAO_USER["email"]
+    mocked_object.find.side_effect = Exception("Database operation failed")
+    sut = UserController(dao=mocked_object)
+
+    with pytest.raises(Exception):
+        sut.get_user_by_email(email)
